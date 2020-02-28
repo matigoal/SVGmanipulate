b540269d399df206a694820b1dbe2977af59e1b5
matigoal <55578343+matigoal@users.noreply.github.com>
2020-02-28 15:52:21 +0100

    Create svg.js

---
+++ svg.js
@@ -0,0 +1,13 @@
+const yop= document.getElementById('matt');
+matt.style.background = '#000888';
+console.table('sucess');
+
+matt.style.position = 'absolute' 
+console.log('nope');
+
+function displayDate() {
+  document.getElementById("matt").replace = console.log(Date());
+}
+
+
+matt.addEventListener("click", displayDate) = console.log(Date());
